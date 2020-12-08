7949307e270a29d1231fd3b06a495eef144f2aad
Anish Agnihotri <contact@anishagnihotri.com>
2020-12-08 11:27:50 -0500

    Trying to remove Apollo Cache

--- apollo/index.js
+++ apollo/index.js
@@ -1,10 +1,8 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client"; // Apollo client
+import { ApolloClient } from "@apollo/client"; // Apollo client
 
 // Create new Apollo client
 const client = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_WP_URL,
-  // With in memory caching
-  cache: new InMemoryCache(),
 });
 
 // Export client
