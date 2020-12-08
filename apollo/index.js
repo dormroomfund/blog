08e7af93ebdb2e43e7727963a734f908b3cad079
Anish Agnihotri <contact@anishagnihotri.com>
2020-12-08 11:36:06 -0500

    Cache busting

--- apollo/index.js
+++ apollo/index.js
@@ -1,8 +1,20 @@
-import { ApolloClient } from "@apollo/client"; // Apollo client
+import { ApolloClient, InMemoryCache } from "@apollo/client"; // Apollo client
 
 // Create new Apollo client
 const client = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_WP_URL,
+  // Setup temporary cache
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "ignore",
+    },
+    query: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "all",
+    },
+  },
 });
 
 // Export client
